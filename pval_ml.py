57fd3ecfbdb3fc6ccea252954d4a863e0ca89683
Chris Kimmel <kimmel.95@osu.edu>
2021-08-13 17:47:11 -0400

    Partially written. Looks far cleaner.

--- pval_ml.py
+++ pval_ml.py
@@ -81,6 +81,7 @@ RANGE_OF_BASES_TO_INCLUDE = (-4, 1) # inclusive
 
 from os import path, makedirs, remove
 from itertools import product
+from Typing import Tuple
 import errno
 
 import numpy as np
@@ -140,7 +141,7 @@ class dataset():
         missing_pos_df = pd.DataFrame({p: np.nan} for p in missing_poss)
         self._data = pd.concat([self._data, missing_pos_df], axis=1) # sorted automatically
 
-    def apply_model(self, linear_svc: smv.LinearSVC, r)
+    def apply_model(self, linear_svc: smv.LinearSVC, r: Tuple[int]) -> pd.DataFrame:
         '''
         Apply a linear support-vector machine to the dataset.
 
@@ -172,6 +173,44 @@ class dataset():
         return pd.DataFrame(predictions, columns=self._data.columns,
             index=self._data.index)
 
+    def restrict_columns(self, lo_incl: int, hi_incl: int) -> None:
+        '''
+        Return the same dataframe, but only with columns lo_incl, lo_incl+1, ...
+        hi_incl-1, hi_incl and no others.
+        '''
+        return self._data.loc[:, lo_incl:hi_incl] # loc uses inclusive indexing
+
+    def to_numpy(self) -> np.ndarray:
+        '''
+        Wrapper for pd.DataFrame.to_numpy()
+        '''
+        return self._data.to_numpy()
+
+    def dropna(self) -> None:
+        '''
+        Wrapper for pd.DataFrame.dropna()
+        '''
+        self._data = self._data.dropna()
+
+def get_model():
+    '''
+    Return an svm.LinearSVC model with parameter choices appropriate for our
+    purposes.
+    '''
+    return svm.LinearSVC(
+        penalty='l2', # default
+        loss='squared_hinge', # default
+        dual=True, # default
+        tol=0.0001, # default
+        C=1.0, # default
+        fit_intercept=True, # default
+        intercept_scaling=1.0, # default
+        class_weight='balanced', # NOT default
+        random_state=855, # NOT default
+        max_iter=1000, # default
+    )
+
+
 ################################################################################
 #################################### Setup #####################################
 ################################################################################
@@ -190,158 +229,68 @@ for filename in ['predictions.csv', 'model_parameters.txt', 'training_results.tx
 
 
 ################################################################################
-############################# Import training data #############################
+################ Estimate model accuracy using cross-validation ################
 ################################################################################
 
-to_concat = []
 for training_pair in TRAINING_PAIR_LIST:
-    # load and format each training pair separately
-    nick = training_pair['model_nickname']
-    ms_0b = training_pair['model_site_0b']
-
-    d = {ms_0b + delta: delta for delta in range(r[0], r[1]+1)}
-    for filepath, positive in [(training_pair['positive_dset_prs_csv'], True),
-                               (training_pair['negative_dset_prs_csv'], False)]:
-        # read features and labels
-        to_concat.append(
-            load_csv(filepath, range(ms_0b + r[0], ms_0b + r[1] + 1))
-            .rename(d, axis=1, errors='raise')
-            .assign(model=nick)
-            .assign(positive=positive)
-            .assign(prediction_site_0b=ms_0b)
-            .dropna()
-        )
-training_dset_df = pd.concat(to_concat).set_index(['model', 'positive', 'prediction_site_0b']).dropna().sort_index()
-del to_concat
-
-
-################################################################################
-############################## Prepare classifier ##############################
-################################################################################
-
-classifier = svm.LinearSVC(
-    penalty='l2', # default
-    loss='squared_hinge', # default
-    dual=True, # default
-    tol=0.0001, # default
-    C=1.0, # default
-    fit_intercept=True, # default
-    intercept_scaling=1.0, # default
-    class_weight='balanced', # NOT default
-    random_state=855, # NOT default
-    max_iter=1000, # default
-)
-
-
-################################################################################
-#### Train on training data with k-fold cross-validation and report results ####
-################################################################################
-
-folder = model_selection.StratifiedKFold(NUMBER_OF_FOLDS, shuffle=True, random_state=855)
-
-for model in TRAINING_PAIR_LIST:
-    model_nick = model['model_nickname']
-    prediction_site_0b = model['model_site_0b']
+    locals().update(training_pair)
+    # above line loads:
+    #   positive_dset_prs_csv, negative_dset_prs_csv, model_site_0b, model_nickname
 
     with open(path.join(OUTPUT_DIRECTORY, 'training_results.txt'), 'at') as f:
         print(f'estimating sensitivity and specificity for {model_nick} model '
               f'using {NUMBER_OF_FOLDS}-fold cross-validation', file=f)
+        print('See https://scikit-learn.org/stable/modules/model_evaluation.html#classification-report')
         print('---', file=f)
 
-    ### convert to numpy ###
-
-    # index levels of training_dset_df are ['model', 'positive', 'prediction_site_0b']
-    pos_X = training_dset_df.loc[idx[model_nick, True, prediction_site_0b], :].to_numpy()
-    neg_X = training_dset_df.loc[idx[model_nick, False, prediction_site_0b], :].to_numpy()
+    pos_X, neg_X = (
+        dataset(filepath).restrict_columns(*r).dropna().to_numpy()
+        for filepath in positive_dset_prs_csv, negative_dset_prs_csv
+    )
     n_pos = pos_X.shape[0]
     n_neg = neg_X.shape[0]
+    y = np.concatenate([np.full((n_pos,), 1),
+                        np.full((n_neg,), 0)], axis=0)
     X = np.concatenate([pos_X, neg_X], axis=0)
-    y = np.concatenate([np.full((n_pos,), 1), np.full((n_neg,), 0)], axis=0)
-
-    ### cross-validate models ###
 
     for train, test in folder.split(X, y):
-        classifier.fit(X[train], y[train])
+        classifier = get_model().fit(X[train], y[train])
         report = metrics.classification_report(y[test], classifier.predict(X[test]))
         with open(path.join(OUTPUT_DIRECTORY, 'training_results.txt'), 'at') as f:
             print(report, file=f)
             print('---', file=f)
 
-
-################################################################################
-######################## Import datasets for evaluation ########################
-################################################################################
-
-features = pd.DataFrame( # previously named "sites"
-    data=product(
-        SITES_TO_EVALUATE_0B,
-        range(RANGE_OF_BASES_TO_INCLUDE[0], RANGE_OF_BASES_TO_INCLUDE[1]+1),
-    ),
-    columns=['prediction_site_0b', 'delta'],
-).assign(pos_0b=lambda x: x['prediction_site_0b'] + x['delta'])
-
-# Each of the possible values of `delta` corresponds to a feature used by the
-# model. The presence of (`pos_0b`, `prediction_site_0b`, `delta`) in the `features`
-# dataframe indicates that, when the model is used to assess whether `prediction_site_0b`
-# is modified, it should use the values at `pos_0b` for feature `delta`.
-
-evaluation_dset_long = pd.concat(
-    longify(load_csv(x['dset_prs_csv'])).assign(dset = x['dset_nickname'])
-    for x in EVALUATION_DATASET_LIST
-)
-# columns: are 'read_id', 'pos_0b', 'pval', 'dset'
-# no index
-
-on_which_to_run_model = (
-    evaluation_dset_long
-    .merge(features, how='inner', on='pos_0b', validate='many_to_many')
-    .pivot(
-        index=['dset', 'read_id', 'prediction_site_0b'],
-        columns='delta',
-        values='pval'
-    ).dropna()
-)
-# Signature of on_which_to_run_model:
-# Index: dset, read_id, prediction_site_0b
-# Columns: -4, -3, -2, -1, 0, 1
+    del pos_X, neg_X, X, y # free up some memory
 
 
 ################################################################################
-################### Train models using ALL the training data ###################
+#################### Train models on full training datasets ####################
 ################################################################################
 
-to_concat = []
-for model in TRAINING_PAIR_LIST:
-    model_nick = model['model_nickname']
-    prediction_site_0b = model['model_site_0b']
+classifiers_and_descriptions = []
+for training_pair in TRAINING_PAIR_LIST:
+    locals().update(training_pair)
+    # above line loads:
+    #   positive_dset_prs_csv, negative_dset_prs_csv, model_site_0b, model_nickname
 
-    print(f'running {model_nick} model on evaluation datasets')
+    with open(path.join(OUTPUT_DIRECTORY, 'training_results.txt'), 'at') as f:
+        print(f'estimating sensitivity and specificity for {model_nick} model '
+              f'using {NUMBER_OF_FOLDS}-fold cross-validation', file=f)
+        print('See https://scikit-learn.org/stable/modules/model_evaluation.html#classification-report')
+        print('---', file=f)
 
-    ### convert training data to numpy ###
-
-    # The 5-fold cross validation models each used 80% of the training data, but
-    # here we use all of it.
-    pos_X = training_dset_df.loc[idx[model_nick, True, prediction_site_0b], :].to_numpy()
-    neg_X = training_dset_df.loc[idx[model_nick, False, prediction_site_0b], :].to_numpy()
+    pos_X, neg_X = (
+        dataset(filepath).restrict_columns(*r).dropna().to_numpy()
+        for filepath in positive_dset_prs_csv, negative_dset_prs_csv
+    )
     n_pos = pos_X.shape[0]
     n_neg = neg_X.shape[0]
+    y = np.concatenate([np.full((n_pos,), 1),
+                        np.full((n_neg,), 0)], axis=0)
     X = np.concatenate([pos_X, neg_X], axis=0)
-    y = np.concatenate([np.full((n_pos,), 1), np.full((n_neg,), 0)], axis=0)
 
-    ### train model and run on evaluation data ###
-
-    classifier.fit(X, y)
-
-    to_concat.append(
-        pd.DataFrame(
-            data=classifier.predict(on_which_to_run_model),
-            index=on_which_to_run_model.index,
-            columns=['predicted']
-        ).assign(model=model_nick)
-        .reset_index()
-    )
-
-    ### save model parameters ###
+    classifier = get_model().fit(X, y)
+    classifiers_and_descriptions.append(classifier, description)
 
     with open(path.join(OUTPUT_DIRECTORY, 'model_parameters.txt'), 'at') as f:
         print(f'MODEL PARAMETERS FOR {model_nick}', file=f)
@@ -349,9 +298,28 @@ for model in TRAINING_PAIR_LIST:
         print(f'classifier intercept:\t\t{classifier.intercept_}', file=f)
         print('---', file=f)
 
-predictions = pd.concat(to_concat, axis=0)
-del to_concat
-# Columns of predictions are: dset, read_id, prediction_site_0b, model, predicted
+    del pos_X, neg_X, X, y # free up some memory
+
+
+################################################################################
+###################### Run models on evaluation datasets #######################
+################################################################################
+
+for evaluation_dataset in EVALUATION_DATASET_LIST:
+    locals().update(evaluation_dataset)
+    # above line loads:
+    #   dset_prs_csv, dset_nickname
+
+    this_dataset = dataset(filepath)
+
+    for classifer, model in classifiers_and_descriptions:
+        locals().update(training_pair)
+        # above line loads:
+        #   positive_dset_prs_csv, negative_dset_prs_csv, model_site_0b, model_nickname
+        lo_incl, hi_incl = (model_site_0b + x for x in r)
+        restricted = this_dataset.restrict(lo_incl, hi_incl)
+        results = restricted.apply_model(classifier)
+
 
 (
     predictions
